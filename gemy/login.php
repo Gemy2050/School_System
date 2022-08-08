6ab8d0cfccf3efdf114bd4d762b21268171ad4c6
Mohamed Omar <gemy46349@gmail.com>
2022-08-09 01:05:44 +0200

    Modified

--- gemy/login.php
+++ gemy/login.php
@@ -21,12 +21,11 @@ if(isset($_POST['submit'])) {
             header('location: /school_system/gemy/test.php');
 
           }else {
-              echo "<style> .forget{diplay: block;} </style>";
-              echo "<h3 class='error'> Password is Error, Try Again </h3>";
+              echo "<h3 class='error'> Password is Wrong, Try Again </h3>";
             }
       }
     } else {
-      echo "<h3 class='error'> Username is Error</h3>";
+      echo "<h3 class='error'> Username is Wrong</h3>";
     }
   }
   ?>
@@ -39,43 +38,6 @@ if(isset($_POST['submit'])) {
         echo "<h2>Click Start To Set Admin</h2>";
         echo "<a class='set' href='signup.php'>Start</a>";
       }
-
-
-        // $to = "gemy46349@gmail.com"; // this is your Email address
-        // $from = "mo2720352@gmail.com"; // this is the sender's Email address
-        // $first_name = "Mohamed";
-        // $last_name = "Gamal Omar";
-        // $subject = "Form submission";
-        // $subject2 = "Copy of your form submission";
-        // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" ."Hello There !!!";
-        // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . "Hello Again!!";
-    
-        // $headers = "From:" . $from;
-        // $headers2 = "From:" . $to;
-        // mail($to,$subject,$message,$headers);
-        // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-        // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-        // // You can also use header('Location: thank_you.php'); to redirect to another page.
-        // // You cannot use header and echo together. It's one or the other.
-
-
-//Mail sending function
-$subject = "Hi";
-$to = "gemy46349@gmail.com";
-$from = "mo2720352@gmail.com";
-
-//data
-$msg = "Your MSG <br>\n";       
-
-//Headers
-$headers  = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=UTF-8\r\n";
-$headers .= "From: <".$from. ">" ;
-
-mail($to,$subject,$msg,$headers);
-echo "Mail Sent.";
-
-
 ?>
 
 <!DOCTYPE html>
@@ -84,21 +46,53 @@ echo "Mail Sent.";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/login.css">
+  <!-- <link rel="stylesheet" href="css/login.css"> -->
   <title>Login</title>
 </head>
+<style>
+<?php include("css/login.css");?>
+
+</style>
 <body>
 
 
 <h2 class="head">Login</h2>
   <form method="POST">
-    <input type="text" name="username" placeholder="Username" class="input" autofocus required>
-    <input type="password" name="password" placeholder="Password" class="input" required>
-    <div class="submit-box" style="position: relative;">
+    <input type="text" name="username" placeholder="Username" class="input" autofocus >
+    <input type="password" name="password" placeholder="Password" class="input" >
     <input type="submit" value="Login" name="submit">
     <a href="../index.php" class="cancel">Cancel</a>
-    <a class="forget" href='#'>forget password </a>
-    </div>
+    <input type="submit" name="forget" value="forget password"><!-- style="padding:0;background:transparent;color:#3AB4F2;font-size:20px"--> 
+  </form>
+
+
+  <?php 
+  if(isset($_POST['forget'])) {
+    echo "<style>.head,form{display: none;} .form2{display:block !important;}</style>";
+    echo "<h2>Login With PIN</h2>";
+  }
+  if(isset($_POST['submit2'])) {
+    $sql = "select username, pin from admin where username = '$_POST[username]'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if($_POST['pin'] == $row['pin']) {
+
+          header('location: /school_system/gemy/test.php');
+
+        }else {
+            echo "<h3 class='error'> PIN is Wrong, Try Again </h3>";
+          }
+    }else {
+      echo "<h3 class='error'> Username is Wrong, Try Again</h3>";
+    }
+  }
+  ?>
+  <form method="POST" style="display: none;" class="form2">
+    <input type="text" name="username" placeholder="Username" class="input" autofocus required>
+    <input type="number" name="pin" placeholder="PIN" class="input" required>
+    <input type="submit" value="Login" name="submit2">
+    <a href="../index.php" class="cancel">Cancel</a>
   </form>
 
 </body>
--- index.php
+++ index.php
@@ -4,18 +4,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/all.min.css">
   <title>school website</title>
   <meta name="description " content="school system">
 </head>
+<style>
+  <?php include ("css/style.css"); ?>
+</style>
 <body>
   <div class="header">
     <div class="container">
       <div class="logo"><img src="images/logo1.jpg" alt=""></div>
       <ul class="nav">
-        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#" style="color: var(--main-color);">Home</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
@@ -25,18 +28,24 @@
 <div class="landing">
   <div class="container">
     <div class="text">
-      <h1>School Time !</h1>
+      <h1>School Time </h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, cum. Itaque at ab ad doloribus.
         Lorem ipsum dolor sit.
     </p>
     </div>
   <div class="image">
-    <img src="images/landing1.webp" alt="" >
+    <img src="images/landing1.webp" alt="Not Found" >
   </div>
   </div>
 </div>
 
 <a href="gemy/login.php">Add Student</a>
+
 </body>
 </html>
+
+<?php 
+
+
+?>
