ee244940f74e7f2c1f2d6e779b676c1a1853389d
Mohamed Omar <gemy46349@gmail.com>
2022-08-13 18:21:39 +0200

    Modified

--- gemy/register.php
+++ gemy/register.php
@@ -1,3 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "students";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if (!$conn)
+  die(mysqli_connect_error());
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,38 +17,228 @@
   <title>register page</title>
 </head>
 <style>
-  <?php include("css/add.css"); ?>
+  body {
+    font-family: system-ui;
+    background-color: #DDD;
+  }
+
+  h2 {
+    margin: auto;
+    margin-top: 15px;
+    margin-bottom: 25px;
+    font-size: 25px;
+    color: #3AB4F2;
+    width: fit-content;
+  }
+
+  form {
+    width: 400px;
+    margin: auto;
+    text-align: center;
+  }
+
+  .input {
+    width: 350px;
+    margin-bottom: 25px;
+    padding: 15px;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #ccc;
+    background-color: #f9f9f9;
+    caret-color: lightseagreen;
+    font-size: 18px;
+  }
+
+  input[type='submit'],
+  a.cancel {
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    margin: 50px auto;
+    font-size: 18px;
+    margin: 10px;
+  }
+
+  input[name="submit"] {
+    background-color: #3AB4F2;
+    border: 2px solid #3AB4F2;
+    color: white;
+    padding: 11px 60px;
+  }
+
+  a.cancel {
+    padding: 10px 60px;
+    background-color: white;
+    color: #3AB4F2;
+    border: 2px solid #3AB4F2;
+    text-decoration: none;
+  }
+
+  .subjects-content {
+    padding: 15px;
+    padding-right: 30px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  h3 {
+    color: #777;
+  }
+
+  .subjects-content .subjects {
+    text-align: left;
+  }
+
+  .subjects-content .subjects .sub {
+    margin: 10px 0;
+    font-size: 18px;
+    user-select: none;
+    color: #3AB4F2;
+    font-weight: bold;
+  }
+
+  input[type='checkbox'] {
+    margin-right: 8px;
+  }
+
+  .warning {
+    background-color: #3AB4F2;
+    width: 450px;
+    height: 250px;
+    font-size: 25px;
+    font-weight: bold;
+    border-radius: 8px;
+    line-height: 250px;
+    color: white;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 111%;
+  background-color: rgba(0, 0, 0, .8);
+  left: 0;
+  top: 0;
+  display: none;
+}
+.home {
+  text-decoration: none;
+  background-color:rgb(25, 141, 243);
+  color: white;
+  font-size: 20px;
+  border-radius: 6px;
+  padding: 10px;
+  position: fixed;
+  left: 4px;
+  top: 10px;
+}
 </style>
 
 <body>
-  <h2>Registration Page</h2>
-  
+<div class="overlay"></div>
+  <div class="container">
+    <h2>Registration Page</h2>
+    <a class="home" href="register_details.php">Register Details</a>
 
-  <form method="post" action="">
-    <span>What are your favourite colours?</span><br/>
-    <input type="checkbox" name='colour[]' value="Red"> Red <br/>
-    <input type="checkbox" name='colour[]' value="Green"> Green <br/>
-    <input type="checkbox" name='colour[]' value="Blue"> Blue <br/>
-    <input type="checkbox" name='colour[]' value="Black"> Black <br/>
-	<br/>
-    <input type="submit" value="Submit" name="submit">
-</form>
+    <form method="post" action="">
 
-<?php
-if(isset($_POST['submit'])){
+      <input class="input" type="number" name="id" placeholder="ID" required>
+      <input class="input" type="text" name="phone" placeholder="Phone" required>
+      <input class="input" type="email" name="email" placeholder="Email" required>
 
-    if(!empty($_POST['colour'])) {
+      <div class="subjects-content">
+        <h3>Select 5 Courses</h3>
+        <div class="subjects">
+          <div class="sub"><input type="checkbox" name='subjects[]' value="Calculus" id="calculus"> <label for="calculus">calculus</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="Physics" id="physics"> <label for="physics">physics</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="English" id="english"> <label for="english">english</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="Algebra" id="algebra"> <label for="algebra">algebra</label> </div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="Algoritms" id="algorithms"><label for="algorithms">algorithms</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="Electronics" id="electronics"> <label for="electronics">electronics</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="IS" id="is"> <label for="is">IS</label></div>
+          <div class="sub"><input type="checkbox" name='subjects[]' value="CS" id="cs"> <label for="cs">CS</label></div>
+        </div>
+      </div>
 
-        foreach($_POST['colour'] as $value){
-            echo "Chosen colour : ".$value.'<br/>';
-        }
+      <input type="submit" value="Add" name="submit">
+      <a href="../index.php" class="cancel">Exit</a>
+    </form>
 
-    }
-
-}
-?>
-
-
+  </div>
 </body>
 
 </html>
+
+<?php
+if (isset($_POST['submit'])) {
+
+  if(strlen($_POST['phone']) != 11 || !is_numeric($_POST['phone'])) {
+    header("REFRESH:2;URL=register.php");
+    echo "<p class='warning'>Please, Phone Must Be 11 Numbers</p>";
+    echo "<style>.overlay{display:block;}</style>"; 
+    return;
+  }
+
+  if (count($_POST['subjects']) == 5) {
+
+
+    $sql = "select name from stds where id = '$_POST[id]'";
+    $result = mysqli_query($conn, $sql);
+
+
+    if (mysqli_num_rows($result) > 0) {
+      $row = mysqli_fetch_assoc($result);
+
+      $name = $row['name'];
+
+      $sub = implode(', ', $_POST['subjects']);
+
+      $sql = "insert into courses values($_POST[id],'$name','$_POST[phone]','$_POST[email]', '$sub')";
+      try {
+        if (!mysqli_query($conn, $sql))
+          throw new Exception();
+          header("REFRESH:2;URL=register_details.php");
+        echo "<p class='warning'>Done Successfully</p>";
+      } catch (Exception $e) {
+        header("REFRESH:2;URL=register_details.php");
+        echo "<p class='warning'>You have Enrolled Once</p>";
+      }
+      echo "<style>.overlay{display:block;}</style>";
+    } else  {
+      header("REFRESH:2;URL=register.php");
+      echo"<p class='warning'>Unknown ID </p>";
+      echo "<style>.overlay{display:block;}</style>";
+    } 
+  } else {
+    header("REFRESH:2;URL=register.php");
+    echo "<p class='warning'>Subjects Must Be 5</p>";
+    echo "<style>.overlay{display:block;}</style>";
+  }
+}
+?>
+
+<?php
+
+// $names = ['Mohamed', 'Ali', 'Zain'];
+
+// $x = implode(', ', $names);
+// echo $x . '<br>';
+
+
+// $a = explode(', ', $x);
+// foreach($a as $b)
+//   echo "<br>" .$b . "<br>";
+
+
+
+
+
+
+// foreach ($_POST['subjects'] as $value) {
+//   echo "Chosen Subjects : " . $value . '<br/>';
+// }
+?>
---
+++ gemy/register_details.php
@@ -0,0 +1,64 @@
+<?php 
+
+  if(!isset($_SERVER['HTTP_REFERER'])){
+    header('location: /school_system/gemy/check.php');
+    exit;
+  }
+
+?>
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "students";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if(!$conn) {
+  die("Connection Error");
+}
+$sql = "select id, name, phone, gender, address from stds";
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/all.min.css">
+  <title>Students Details</title>
+</head>
+<style>
+
+</style>
+<body>
+  <a class="add" href="../index.php">Return Main </a>
+  <table>
+        <?php 
+        $sql = "select id, name, phone, email, subjects from courses order by name";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+          echo "<thead>
+          <tr>
+          <td>ID</td>
+          <td>Student Name</td>
+            <td>Phone</td>
+            <td>Email</td>
+            <td>Subjects</td>
+            </tr>
+          </thead>";
+          while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['name']."</td>";
+            echo "<td> 0".$row['phone']."</td>";
+            echo "<td>".$row['email']."</td>";
+            echo "<td>".$row['subjects']."</td>";
+            echo "</tr>";
+          } 
+        }else echo "<h2 class='empty'>Table is Empty</h2>";
+        ?>
+  </table>
+</body>
+</html>
